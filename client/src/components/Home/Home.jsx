e8dbf7f1ab1eadab99ec15618ce8108024fd66af
Aryan Daftari <aryandaftari7@gmail.com>
2024-06-03 23:14:49 +0530

    Issue Solved

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,34 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
-import {BounceLoader} from 'react-spinners'
 import SignUp from '../SignUp/SignUp';
 import Login from '../Login/Login';
+import axios from 'axios';
+import Shimmer from '../Shimmer/Shimmer';
 import './Home.css';
 
 const Home = () => {
   const [isSignUp, setIsSignUp] = useState(true);
   const [isLogin, setIsLogin] = useState(false);
-  const[loading , setLoading] = useState(true)
+  const [loading , setLoading] = useState(false);
   
   const { setUsername, setId, setIsUserLoggedIn, isUserLoggedIn } = useContext(UserContext);
   const navigate = useNavigate();
-
-  const loadingSpinnerStyles = {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 9999,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    transform:"none"
-  
-  }
-  
 
   const handleSignUpSuccess = () => {
     setIsSignUp(false);
@@ -47,39 +32,39 @@ const Home = () => {
 
   useEffect(() => {
     setLoading(true);
-    if (isUserLoggedIn) {
-      navigate('/app/dashboard');
-    }
-    setLoading(false)
-  }, [isUserLoggedIn, navigate]);
+    const getUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        console.log("Token -", token);
+        if (token) {
+          axios.defaults.headers.common["Authorization"] = token;
+          const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/user/profile`);
+          setIsUserLoggedIn(true);
+          setUsername(response.data.message.name);
+          setId(response.data.message._id);
+          setLoading(false);
+          navigate('/app/dashboard');
+        } else {
+          setIsUserLoggedIn(false);
+          setUsername(null);
+          setId(null);
+          setLoading(false); // Set loading to false when no user is logged in
+        }
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+    };
+    getUser();
+  }, [isUserLoggedIn, navigate, setUsername, setId, setIsUserLoggedIn]);
 
-
+  if (loading) {
+    return <Shimmer/>; // Render nothing when loading
+  }
 
   return (
-    <div>
-      {loading ? 
-      <>      <div className="loading-spinner" >
-      <BounceLoader
-        color={"#ffffff"}
-        loading={loading}
-        cssOverride={loadingSpinnerStyles}
-        size={150}
-        aria-label="Loading Spinner"
-      />
-    </div>
-
-      
-      </>
-      
-    :
-    
-    <>
-          <div className='login-sign-up-container'>
-        {isSignUp && <SignUp handleSignUpSuccess={handleSignUpSuccess} clickLogin={clickLogin} clickSignUp={clickSignUp} />}
-        {isLogin && <Login clickLogin={clickLogin} clickSignUp={clickSignUp} />}
-      </div>
-
-    </>}
+    <div className='login-sign-up-container'>
+      {isSignUp && <SignUp handleSignUpSuccess={handleSignUpSuccess} clickLogin={clickLogin} clickSignUp={clickSignUp} />}
+      {isLogin && <Login clickLogin={clickLogin} clickSignUp={clickSignUp} />}
     </div>
   );
 };

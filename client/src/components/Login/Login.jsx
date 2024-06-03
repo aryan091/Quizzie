27cc77dec729f26ded1304152e998beb6be18133
Aryan Daftari <aryandaftari7@gmail.com>
2024-06-03 20:34:19 +0530

    Updated

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -27,7 +27,7 @@ const Login = (props) => {
       // Navigate to the dashboard
       navigate('/app/dashboard');
     } catch (error) {
-      console.log(error.response.data.message);
+      console.log(error);
       setErrors(error.response.data.message);
     } finally {
       setLoading(false); // Set loading back to false when API call completes

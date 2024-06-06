862c19473502888d9d865a9a92a490013d582200
Aryan Daftari <aryandaftari7@gmail.com>
2024-06-06 21:27:56 +0530

    Fixed Sign Up viewing

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ import './Home.css';
 const Home = () => {
   const [isSignUp, setIsSignUp] = useState(true);
   const [isLogin, setIsLogin] = useState(false);
-  const [loading , setLoading] = useState(false);
+  const [loading , setLoading] = useState(true);
   
   const { setUsername, setId, setIsUserLoggedIn, isUserLoggedIn } = useContext(UserContext);
   const navigate = useNavigate();
@@ -39,23 +39,28 @@ const Home = () => {
         if (token) {
           axios.defaults.headers.common["Authorization"] = token;
           const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/user/profile`);
-          setIsUserLoggedIn(true);
           setUsername(response.data.message.name);
           setId(response.data.message._id);
-          setLoading(false);
           navigate('/app/dashboard');
         } else {
           setIsUserLoggedIn(false);
           setUsername(null);
           setId(null);
-          setLoading(false); // Set loading to false when no user is logged in
         }
       } catch (error) {
         console.log(error.response.data.message);
       }
+      finally {
+        setLoading(false); // Set loading back to false when API call completes
+      }
     };
     getUser();
   }, [isUserLoggedIn, navigate, setUsername, setId, setIsUserLoggedIn]);
+
+  if(loading)
+    {
+      return <Shimmer />
+    }
 
   return (
     <div className='login-sign-up-container'>
